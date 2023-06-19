915ff90a07206663544491bee477e54b7c497bea
amrutapawar1 <amrutapawar800@gmail.com>
2023-06-19 13:27:29 +0530

    first commit

---
+++ contact.php
@@ -0,0 +1,59 @@
+<?php
+
+
+ $name = $_POST['name'];
+$email = $_POST['email'];
+$contact = $_POST['modal_my_mobile2'];
+$country_code = $_POST['country_code'];
+$country_name = $_POST['country_name'];
+
+
+$to = "Sur.mulla@gmail.Com";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>Name&#58; $name</tr>
+<tr>Email&#58; $email</tr>
+<tr>Phone&#58; $contact</tr>
+<tr>country_code&#58; $country_code</tr>
+<tr>country_name&#58; $country_name</tr>
+</table>
+</body>
+</html>
+";
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <no_reply@jhamtani.com>' . "\r\n";
+$headers .= 'Cc: ' . "\r\n";
+
+
+
+if(mail($to,$subject,$message,$headers)) //Send an Email. Return true on success or false on error
+
+
+{
+echo 
+
+"<script>window.location.href='thank-you.html';</script>";
+}
+else
+{
+echo "<script>
+alert('Plz Try Agian');
+window.location.href='index.html'
+
+;
+</script>";
+}
+?>
